23a2ae5129e06413d9ebaaa48a4795fedd3c6707
Marvin Meng <marvinmeng11@gmail.com>
2021-01-05 20:57:17 -0500

    Added routes for meetings and ideas

--- server/ideasRouter.js
+++ server/ideasRouter.js
@@ -1,4 +1,41 @@
 const express = require('express');
 const ideasRouter = express.Router();
+const dbFunctions = require("./db");
+
+ideasRouter.get('/', (req, res, next) => {
+	res.send(dbFunctions.getAllFromDatabase('ideas'));
+});
+
+ideasRouter.post('/', (req, res, next) => {
+	addToDatabase('ideas', req.query);
+	res.status(201).send('Idea Created Successfully');
+});
+
+ideasRouter.get('/:ideaId', (req, res, next) => {
+	if (dbFunctions.getFromDatabaseById('ideas', req.params.ideaId)) {
+		res.send(dbFunctions.getFromDatabaseById('ideas', req.params.ideaId));
+	} else {
+		res.status(404).send("Idea with that ID does not exist");
+	}
+});
+
+ideasRouter.put('/:ideaId', (req, res, next) => {
+	if (dbFunctions.getFromDatabaseById('ideas', req.params.ideaId)) {
+		dbFunctions.getFromDatabaseById('ideas', req.params.ideaId) = req.query;
+		res.send(`Idea ${req.params.id} updated successfully`);
+	} else {
+		res.status(404).send("Idea with that ID does not exist");
+	}
+});
+
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+	if (deleteFromDatabasebyId('ideas', req.params.ideaId)) {
+		res.status(204).send();
+	} else {
+		res.status(404).send(`Idea with ID ${req.params.ideaId} not found`);
+	}
+})
+
+
 
 module.exports = ideasRouter;
--- server/meetingsRouter.js
+++ server/meetingsRouter.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const meetingsRouter = express.Router();
+const dbFunctions = require("./db");
+
+meetingsRouter.get('/', (req, res, next) => {
+	res.send(dbFunctions.getAllFromDatabase('meetings'));
+});
+
+meetingsRouter.post('/', (req, res, next) => {
+	addToDatabase('meetings', req.query);
+	res.status(201).send('Meeting Created Successfully');
+});
+
+
+
+meetingsRouter.delete('/', (req, res, next) => {
+	db.deleteAllFromDatabase('meetings');
+})
+
 
 module.exports = meetingsRouter;
